038adb2318eb1f0d50243b016820d14f2f231731
nataly-enne <ennenataly@gmail.com>
2019-10-16 15:39:52 -0300

    create: Tbinary and exemple text on main (just codes)

---
+++ src/br/com/imd/edb2/nataly/main/Main.java
@@ -0,0 +1,21 @@
+package br.com.imd.edb2.nataly.main;
+
+import br.com.imd.edb2.nataly.application.*;;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args[0].equals("compress")) {
+            new Compressor(args[1], args[2], args[3]);
+
+        } 
+        else if (args[0].equals("extract")) {
+            new Extractor(args[1], args[2], args[3]);
+
+        }
+
+    }
+}
