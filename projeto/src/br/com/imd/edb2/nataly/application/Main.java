5443a6ddb98321472b8c38539f90700b9b860532
nataly-enne <ennenataly@gmail.com>
2019-10-22 09:35:51 -0300

    fix: code lines on main // add: readme

--- projeto/src/br/com/imd/edb2/nataly/application/Main.java
+++ projeto/src/br/com/imd/edb2/nataly/application/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // para comprimir: java -jav projeto.jar compress "local do aquivo de texto" "local e novo nome para aquivo binario" "local e novo nome para arquivo de chave"
+
         if (args[0].equals("compress")) {
             Compressor compressor = new Compressor(args[1], args[2], args[3]);
 
@@ -15,7 +15,6 @@ public class Main {
             compressor.textEncoded();
 
         }
-        // para extract: java -jav projeto.jar compress "local aquivo binario" "local arquivo de chave" "local e novo nome aquivo de texto"
         else if (args[0].equals("extract")) {
             Extractor extractor = new Extractor(args[1], args[2], args[3]);
 
